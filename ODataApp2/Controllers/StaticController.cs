0bdfb88fe12452420ae5e48a625a97011ff0c67b
Mutlu Kokdemir <mutlu.kokdemir@piworks.net>
2021-08-11 13:46:28 +0300

    Added GetTaxRateByCategory(Unbound) function.

---
+++ ODataApp2/Controllers/StaticController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ODataApp2.Controllers
+{
+    public class StaticController : ODataController
+    {
+        [HttpGet]
+        [ODataRoute("GetTaxRateByCategory(Category={categoryId})")]
+        public IHttpActionResult GetTaxRateByCategory([FromODataUri] int categoryId)
+        {
+            double rate = 0.0;
+            switch (categoryId)
+            {
+                case 1:
+                    rate = 0.18;
+                    break;
+                default:
+                    break;
+            }
+
+            return Ok(rate);
+        }
+    }
+}
